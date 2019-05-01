37b4080be2a4880a0e0c5e3c7e5a085af6699f5f
Tân Dv <mchemistry95@gmail.com>
2019-05-01 19:48:33 +0700

    Deploy server

--- server.js
+++ server.js
@@ -11,10 +11,12 @@ const cors       = require('cors');
 app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname,'')));
+app.use(express.static(__dirname + '/public'));
 const send = require('./router/sendmail');
 app.use(config.API_PATH, send);
-
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, './public', 'index.html'));
+})
 app.listen(config.PORT, () =>{
     console.log('Server is running on ' + config.getHTTPUrl());
 });
